1af924f6255559da90587a56a656be9dc1f53a19
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2025-09-02 16:56:32 +0500

    setup express server

--- index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config({
-    path: "./.env"
-});
-
-const PORT = process.env.PORT || 3000;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+import connectDb
+ from './db/index.js';
+dotenv.config({
+    path: './.env'
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+connectDb().then(()=>{
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+}).catch((error)=>{
+    console.error("Failed to connect to the database", error);
+    process.exit(1);
+});
